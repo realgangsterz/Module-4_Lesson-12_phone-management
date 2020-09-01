f60c8f686b2ec0c824adbda51e18bf15095aaaee
daoxuandong1997 <daoxuandong1997@gmail.con>
2020-09-01 16:59:40 +0700

    chú thích

---
+++ src/main/java/com/codegym/controller/SmartphoneController.java
@@ -0,0 +1,68 @@
+package com.codegym.controller;
+
+import com.codegym.model.Smartphone;
+import com.codegym.service.SmartphoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/smartphones")
+public class SmartphoneController {
+
+    @Autowired
+    private SmartphoneService smartphoneService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Iterable<Smartphone> allPhones(){
+        return smartphoneService.findAll();
+    }
+
+    @GetMapping("")
+    public ModelAndView allPhonesPage() {
+        ModelAndView modelAndView = new ModelAndView("phones/all-phones");
+
+        modelAndView.addObject("allphones", allPhones());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public ModelAndView createSmartphonePage() {
+        ModelAndView mav = new ModelAndView("phones/new-phone");
+        mav.addObject("sPhone", new Smartphone());
+        return mav;
+    }
+
+    @RequestMapping(value = "/create-newPhone", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Smartphone createSmartphone(@RequestBody Smartphone smartphone) {
+        //@RequestBody Smartphone smartphone thực hiện gán dữ liệu từ json nhận được vào các trường tương ứng của smartphone
+        return smartphoneService.save(smartphone);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Smartphone deleteSmartphone(@PathVariable Integer id){
+        return smartphoneService.remove(id);
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView editSmartphonePage(@PathVariable int id) {
+        ModelAndView mav = new ModelAndView("phones/edit-phone");
+        Smartphone smartphone = smartphoneService.findById(id);
+        mav.addObject("sPhone", smartphone);
+        return mav;
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Smartphone editSmartphone(@PathVariable int id, @RequestBody Smartphone smartphone) {
+        smartphone.setId(id);
+        return smartphoneService.save(smartphone);
+    }
+}
---
+++ src/main/java/com/codegym/model/Produce.java
@@ -0,0 +1,14 @@
+//package com.codegym.model;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "produces")
+//public class Produce {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private
+//}
